a989b1fa3a9f05589c1b8232db053a996eb4b262
Nabil Pervez <perveznabil@gmail.com>
2020-10-14 21:16:19 -0500

    fixed code

--- src/App.js
+++ src/App.js
@@ -3,14 +3,6 @@ import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 import Directory from './components/DirectoryComponent';
 
-// added these like told in the class. i have a feeling this isn't correct
-// error started showing Line 0:  Parsing error: Invalid array length after added.
-</Navbar>
-<Directory />
-
-// when navbar and directory removed the following error shows. even if directory isnt called.
-//./src/components/DirectoryComponent.js Line 52:19:  Parsing error: Unterminated JSX contents
-
 class App extends Component {
     render() {
         return (
@@ -20,6 +12,7 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory />
             </div>
         );
     }
