d4387987eeb92d1656574af8913fb691e826b527
Nabil Pervez <perveznabil@gmail.com>
2020-11-03 21:44:44 -0600

    react redux forms revisit

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -5,7 +5,8 @@ import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
-
+import { createForms } from 'react-redux-form';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -13,8 +14,11 @@ export const ConfigureStore = () => {
             campsites: Campsites,
             comments: Comments,
             partners: Partners,
-            promotions: Promotions
-        }),
+            promotions: Promotions,
+            ...createForms({
+                feedbackForm: InitialFeedback
+            })
+        }), 
         applyMiddleware(thunk, logger)
     );
 
