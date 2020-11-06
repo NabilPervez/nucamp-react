9659f42ee20e17d62b6fb666a6e7cff0eda75423
Nabil Pervez <perveznabil@gmail.com>
2020-11-05 21:06:56 -0600

    fetch

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -5,7 +5,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap-social/bootstrap-social.css';
 import { Loading } from './LoadingComponent';
-
+import { baseUrl } from '../shared/baseUrl';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -108,10 +108,10 @@ function RenderCampsite({campsite}){
     return(
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
-                <CardBody>
-                    <CardText>{campsite.description}</CardText>
-                </CardBody>
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />  
+                    <CardBody>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
             </Card>
         </div>
         );
