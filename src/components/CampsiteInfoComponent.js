1c934a41cfb61f9e3ec81eab39ffd7315161d4d0
Nabil Pervez <perveznabil@gmail.com>
2020-11-05 21:23:59 -0600

    fetch post comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -35,7 +35,7 @@ class CommentForm extends React.Component{
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
 
@@ -117,7 +117,7 @@ function RenderCampsite({campsite}){
         );
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
     if(comments){
         return(
             <div className="col-md-5 m-1">
@@ -131,7 +131,7 @@ function RenderComments({comments, addComment, campsiteId}) {
                     </div>
                     )
                     })}
-                <CommentForm campsiteId={campsiteId} addComment={addComment} />
+                <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
 
         );
@@ -177,9 +177,9 @@ function CampsiteInfo(props) {
 
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
+                    <RenderComments
                         comments={props.comments}
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
                 </div>
